1b10a965ecf52c58e3dd71201f33964a85040691
sergiop45 <sergiopsantos33@gmail.com>
2022-10-18 22:10:57 -0300

    configuracao-inicial

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USER,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    synchronize: true,
+  } as TypeOrmModuleOptions),
+    UsersModule,],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
